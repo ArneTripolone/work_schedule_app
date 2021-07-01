d606817fd8c643c0abee61150eda8c63448a6d5e
Arne Tripolone <arnetripolone@gmail.com>
2021-07-01 13:52:33 +1000

    Now with input elements

--- script.js
+++ script.js
@@ -1,103 +1,104 @@
 var time = moment().format("MMM Do, YYYY");
 $("#currentDay").text(time);
 
-var beginningTime9 = moment('9:00am', 'h:mma');
-var endTime9 = moment('9:59am', 'h:mma');
+var beginningTime9 = moment('09:00', 'h:mma');
+var endTime9 = moment('09:59', 'h:mma');
 
-var beginningTime10 = moment('10:00am', 'h:mma');
-var endTime10 = moment('10:59am', 'h:mma');
+var beginningTime10 = moment('10:00', 'h:mma');
+var endTime10 = moment('10:59', 'h:mma');
 
-var beginningTime11 = moment('11:00am', 'h:mma');
-var endTime11 = moment('11:59am', 'h:mma');
+var beginningTime11 = moment('11:00', 'h:mma');
+var endTime11 = moment('11:59', 'h:mma');
 
-var beginningTime12 = moment('12:00pm', 'h:mma');
-var endTime12 = moment('12:59pm', 'h:mma');
+var beginningTime12 = moment('12:00', 'h:mma');
+var endTime12 = moment('12:59', 'h:mma');
 
-var beginningTime1 = moment('1:00pm', 'h:mma');
-var endTime1 = moment('1:59pm', 'h:mma');
+var beginningTime1 = moment('13:00', 'h:mma');
+var endTime1 = moment('13:59', 'h:mma');
 
-var beginningTime2 = moment('2:00pm', 'h:mma');
-var endTime12 = moment('2:59pm', 'h:mma');
+var beginningTime2 = moment('14:00', 'h:mma');
+var endTime12 = moment('14:59', 'h:mma');
 
-var beginningTime3 = moment('3:00pm', 'h:mma');
-var endTime3 = moment('3:59pm', 'h:mma');
+var beginningTime3 = moment('15:00', 'h:mma');
+var endTime3 = moment('15:59', 'h:mma');
 
-var beginningTime4 = moment('4:00pm', 'h:mma');
-var endTime4 = moment('4:59pm', 'h:mma');
+var beginningTime4 = moment('16:00', 'h:mma');
+var endTime4 = moment('16:59', 'h:mma');
 
-var beginningTime5 = moment('5:00pm', 'h:mma');
-var endTime5 = moment('5:59pm', 'h:mma');
+var beginningTime5 = moment('17:00', 'h:mma');
+var endTime5 = moment('17:59', 'h:mma');
+
 
 if ((moment().isAfter(endTime9)))
 {
-    document.getElementById("nine").className = "past";
+    document.getElementById("nine").classList.add("past");
 
 }else {
-    document.getElementById("nine").className = "future";
+    document.getElementById("nine").classList.add("future");
 }
 
 if ((moment().isAfter(endTime10)))
 {
-    document.getElementById("ten").className = "past";
+    document.getElementById("ten").classList.add("past");
 
 }else {
-    document.getElementById("ten").className = "future";
+    document.getElementById("ten").classList.add("future");
 }
 
 if ((moment().isAfter(endTime11)))
 {
-    document.getElementById("eleven").className = "past";
+    document.getElementById("eleven").classList.add("past");
 
 }else {
-    document.getElementById("eleven").className = "future";
+    document.getElementById("eleven").classList.add("future");
 }
 
 if ((moment().isAfter(endTime12)))
 {
-    document.getElementById("twelve").className = "past";
+    document.getElementById("twelve").classList.add("past");
 
 }else {
-    document.getElementById("twelve").className = "past";
+    document.getElementById("twelve").classList.add("future");
 }
 
 if ((moment().isAfter(endTime1)))
 {
-    document.getElementById("one").className = "past";
+    document.getElementById("one").classList.add("past");
 
 }else {
-    document.getElementById("one").className = "past";
+    document.getElementById("one").classList.add("future");
 }
 
 if ((moment().isAfter(endTime2)))
 {
-    document.getElementById("two").className = "past";
+    document.getElementById("two").classList.add("past");
 
 }else {
-    document.getElementById("two").className = "past";
+    document.getElementById("two").classList.add("future");
 }
 
 if ((moment().isAfter(endTime3)))
 {
-    document.getElementById("three").className = "past";
+    document.getElementById("three").classList.add("past");
 
 }else {
-    document.getElementById("three").className = "past";
+    document.getElementById("three").classList.add("future");
 }
 
 if ((moment().isAfter(endTime4)))
 {
-    document.getElementById("four").className = "past";
+    document.getElementById("four").classList.add("past");
 
 }else {
-    document.getElementById("four").className = "past";
+    document.getElementById("four").classList.add("future");
 }
 
 if ((moment().isAfter(endTime5)))
 {
-    document.getElementById("five").className = "past";
+    document.getElementById("five").classList.add("past");
 
 }else {
-    document.getElementById("five").className = "past";
+    document.getElementById("five").classList.add("future");
 }
 
 
