3b1de9f7784bc2931347536d77c9397e334bba3f
Arne Tripolone <arnetripolone@gmail.com>
2021-07-02 13:04:27 +1000

    Final solution! Now local storage saves and displays data!

--- script.js
+++ script.js
@@ -126,12 +126,66 @@ if ((moment().isAfter(endTime5)))
     document.getElementById("five").classList.add("past");
 }
 
-//function to save the value to local storage
-function save(){
+//functions to save text input to local storage
+function save9(){
     var dataToSave = document.getElementById("myInput9").value;
-    localStorage.setItem("data", dataToSave);
+    localStorage.setItem("data9", dataToSave);
 }
 
-window.onbeforeunload = function() {
-    localStorage.setItem("data", $('#myInput9').val());
+$("#myInput9").val(localStorage.getItem("data9"));
+
+function save10(){
+    var dataToSave = document.getElementById("myInput10").value;
+    localStorage.setItem("data10", dataToSave);
 }
+
+$("#myInput10").val(localStorage.getItem("data10"));
+
+function save11(){
+    var dataToSave = document.getElementById("myInput11").value;
+    localStorage.setItem("data11", dataToSave);
+}
+
+$("#myInput11").val(localStorage.getItem("data11"));
+
+function save12(){
+    var dataToSave = document.getElementById("myInput12").value;
+    localStorage.setItem("data12", dataToSave);
+}
+
+$("#myInput12").val(localStorage.getItem("data12"));
+
+function save1(){
+    var dataToSave = document.getElementById("myInput1").value;
+    localStorage.setItem("data1", dataToSave);
+}
+
+$("#myInput1").val(localStorage.getItem("data1"));
+
+function save2(){
+    var dataToSave = document.getElementById("myInput2").value;
+    localStorage.setItem("data2", dataToSave);
+}
+
+$("#myInput2").val(localStorage.getItem("data2"));
+
+function save3(){
+    var dataToSave = document.getElementById("myInput3").value;
+    localStorage.setItem("data3", dataToSave);
+}
+
+$("#myInput3").val(localStorage.getItem("data3"));
+
+function save4(){
+    var dataToSave = document.getElementById("myInput4").value;
+    localStorage.setItem("data4", dataToSave);
+}
+
+$("#myInput4").val(localStorage.getItem("data4"));
+
+function save5(){
+    var dataToSave = document.getElementById("myInput5").value;
+    localStorage.setItem("data5", dataToSave);
+}
+
+$("#myInput5").val(localStorage.getItem("data5"));
